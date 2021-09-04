66a5b087c84e7a42bcce7539fe7d56c0b1c7e822
Totobriac <totobriac@gmail.com>
2021-09-04 20:37:24 +0200

    clouds done

--- js/plane_level/clouds.js
+++ js/plane_level/clouds.js
@@ -1,11 +1,10 @@
 const cloud = new Image();
 cloud.src = "../assets/plane_level/cloud.png";
-
-const sWidth = 200;
-const sHeight = 100;
+const sWidth = 110;
+const sHeight = 60;
 
 const origin = { x: 600, y: 1200 };
-const radius = 1000;
+const radius = 900;
 const start = 0.9272951769;
 const end = 2.21429922;
 
@@ -14,39 +13,62 @@ const totalSteps = 1220;
 const stepSize = (end - start) / totalSteps;
 
 let cloudsArray = []
+const cloudColors = ['#83857e', '#acadaa', '#cecfcc'];
+
 
 class Cloud {
   constructor(step) {
-    this.step = step
+    this.step = step;
+    this.r1;
+    this.r2;
+    this.r3;
+    this.color;
+    this.createCloud();
+  }
+  createCloud() {
+    this.r1 = 25 + Math.random() * 20;
+    this.r2 = 25 + Math.random() * 10;
+    this.r3 = 25 + Math.random() * 5;
+    this.color = cloudColors[Math.floor(Math.random() * 3)];
   }
 }
 
 export function animateCloud(ctx, frame) {
 
-  if (frame % 300 === 0) {
+  if (frame % 160 === 0) {
     cloudsArray.unshift(new Cloud(0))
+    console.log(cloudsArray);
   }
-
-  if (cloudsArray.length > 5) {
+  if (cloudsArray.length > 7) {
     cloudsArray.pop(cloudsArray[0])
   }
-
   for (let i = 0; i < cloudsArray.length; i++) {
-    
     if (cloudsArray[i].step === totalSteps) cloudsArray[i].step = 0;
     const angle = start + cloudsArray[i].step++ * stepSize;
-
     ctx.translate(origin.x, origin.y);
     ctx.rotate(-angle);
-
     ctx.translate(radius, 0);
     ctx.rotate(Math.PI / 2);
-
     ctx.translate(0, 0)
+    ctx.fillStyle = 'red';
+
     ctx.fillRect(0, 0, 3, 3);
 
-    ctx.translate(-sWidth / 2, 100);
-    ctx.drawImage(cloud, 0, 0, sWidth, sHeight);
+    ctx.fillStyle = cloudsArray[i].color;
+    ctx.fillRect(-35, 50, 70, 20);
+    drawCircle(-35, 60, 10, ctx);
+    drawCircle(35, 55, cloudsArray[i].r1, ctx);
+    drawCircle(0, 55, cloudsArray[i].r2, ctx);
+    drawCircle(-10, 60, cloudsArray[i].r3, ctx);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(-40, 70, 115, 30);
+
     ctx.resetTransform();
-  }  
+  }
+}
+
+function drawCircle(cx, cy, radius, ctx) {
+  ctx.beginPath();
+  ctx.arc(cx, cy, radius, 0, 2 * Math.PI);
+  ctx.fill();
 }
