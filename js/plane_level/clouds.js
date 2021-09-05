daf5479d9e210b3eef3d64ec7c56df2e99a2678c
Your Name <you@example.com>
2021-09-05 17:20:01 +0200

    new cloud

--- js/plane_level/clouds.js
+++ js/plane_level/clouds.js
@@ -1,5 +1,5 @@
-const sWidth = 110;
-const sHeight = 60;
+const earthSprite = new Image();
+earthSprite.src = "../assets/plane_level/cloud_fancy.png"
 
 const origin = { x: 600, y: 1200 };
 const radius = 900;
@@ -21,48 +21,54 @@ class Cloud {
     this.r2;
     this.r3;
     this.color;
+    this.h;
     this.createCloud();
   }
   createCloud() {
     this.r1 = 25 + Math.random() * 20;
     this.r2 = 25 + Math.random() * 10;
     this.r3 = 25 + Math.random() * 5;
+    this.h = Math.random() * 30;
     this.color = cloudColors[Math.floor(Math.random() * 3)];
   }
 }
 
 export function animateCloud(ctx, frame) {
-  ctx.translate(origin.x, origin.y);
-  drawCircle(0, 50, 900, ctx);
-  ctx.fillStyle="#acadaa";
-  ctx.fill();
+
   if (frame % 160 === 0) {
     cloudsArray.unshift(new Cloud(0))
   }
   if (cloudsArray.length > 8) {
     cloudsArray.pop(cloudsArray[0])
   }
-  for (let i = 0; i < cloudsArray.length; i++) {
-    if (cloudsArray[i].step === totalSteps) cloudsArray[i].step = 0;
-    const angle = start + cloudsArray[i].step++ * stepSize;
-    ctx.translate(origin.x, origin.y);
-    ctx.rotate(-angle);
-    ctx.translate(radius, 0);
-    ctx.rotate(Math.PI / 2);
-    ctx.translate(0, 0);   
-    drawCloud(ctx, i);   
-  }
+  // for (let i = 0; i < cloudsArray.length; i++) {
+  //   if (cloudsArray[i].step === totalSteps) cloudsArray[i].step = 0;
+  //   const angle = start + cloudsArray[i].step++ * stepSize;
+  //   ctx.translate(origin.x, origin.y);
+  //   ctx.rotate(-angle);
+  //   ctx.translate(radius, 0);
+  //   ctx.rotate(Math.PI / 2);
+  //   ctx.translate(0, 0);
+  //   drawCloud(ctx, i, cloudsArray[i].h);
+  // }
+  ctx.translate(origin.x, origin.y);
+  drawCircle(0, 50, 900, ctx);
+  ctx.fillStyle = "#acadaa";
+  ctx.fill();
+  ctx.resetTransform();
+
+  ctx.drawImage(earthSprite, 0, 0, 1200, 400, 0, 0, 1200, 400)
 }
 
-function drawCloud(ctx, i) {
+function drawCloud(ctx, i, h) {
   ctx.fillStyle = cloudsArray[i].color;
-  ctx.fillRect(-35, 0, 70, 20);
-  drawCircle(-35, 10, 10, ctx);
-  drawCircle(35, 5, cloudsArray[i].r1, ctx);
-  drawCircle(0, 5, cloudsArray[i].r2, ctx);
-  drawCircle(-10, 10, cloudsArray[i].r3, ctx);
+  ctx.fillRect(-35, -10 + h, 70, 20);
+  drawCircle(-35, 0 + h, 10, ctx);
+  drawCircle(35, -5 + h, cloudsArray[i].r1, ctx);
+  drawCircle(0, -5 + h, cloudsArray[i].r2, ctx);
+  drawCircle(-10, 0 + h, cloudsArray[i].r3, ctx);
   ctx.fillStyle = 'white';
-  ctx.fillRect(-40, 20, 115, 30);
+  ctx.fillRect(-40, 10 + h, 115, 30);
   ctx.resetTransform();
 }
 
