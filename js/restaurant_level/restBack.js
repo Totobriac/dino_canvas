d091a03540310cba9eb54680fbc32ad79f254a43
Toto Briac <totobriac@gmail.com>
2021-09-07 20:57:24 +0200

    no window

--- js/restaurant_level/restBack.js
+++ js/restaurant_level/restBack.js
@@ -1,5 +1,5 @@
 const restBack = new Image();
-restBack.src = "../assets/restaurant_level/restaurant_sm.png"
+restBack.src = "../assets/restaurant_level/restaurant_no_window.png"
 
 export function generateRestBack(ctx) {
   ctx.drawImage(restBack, 0, 0, 600, 200, 0, 0, canvas.width, canvas.height)
