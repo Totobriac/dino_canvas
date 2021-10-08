e093b8e8ad396f6da4e8bd329e1d7d5fce71bff7
Your Name <you@example.com>
2021-10-08 16:05:06 +0200

    actions looking fine

--- js/mansion_level/actions.js
+++ js/mansion_level/actions.js
@@ -2,12 +2,17 @@ var actionsList = ["Pousser", "Tirer", "Ouvrir", "Fermer", "Lire",
   "Prendre", "Utiliser", "Allumer", "Eteindre", "Regarder"]
 
 var actions = [];
+var maxTick = 3;
+var tick = 0;
+var filter = 0;
+var oldSelection;
 
 class Action {
   constructor(action, x, y) {
     this.action = action;
     this.y = y;
     this.x = x;
+    this.filter = "none";
   }
 }
 
@@ -16,12 +21,12 @@ function createActions() {
   var aY2 = 60;
   for (let i = 0; i < actionsList.length; i++) {
     if (i <= 4) {
-      var act = new Action(actionsList[i], 900, aY1);
+      var act = new Action(actionsList[i], 880, aY1);
       actions.push(act);
       aY1 += 60;
     }
     else {
-      var act = new Action(actionsList[i], 1050, aY2);
+      var act = new Action(actionsList[i], 1030, aY2);
       actions.push(act);
       aY2 += 60;
     }
@@ -35,21 +40,36 @@ export function drawActions(ctx, game) {
     createActions();
     game.level8Started = true;
   }
-
   for (let i = 0; i < actions.length; i++) {
-    ctx.fillStyle = "grey";
-    ctx.fillRect(actions[i].x - 10, actions[i].y - 30, 145, 45);
-    ctx.fillStyle = "white";
-    ctx.font = "30px Arial";
-    ctx.fillText(actions[i].action, actions[i].x, actions[i].y);
+    ctx.filter = "url(#turb" + actions[i].filter + ")";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.strokeRect(actions[i].x , actions[i].y - 30, 145, 45);
+    ctx.fillStyle = "black";
+    ctx.font = "30px Garamond";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(actions[i].action, actions[i].x + 70, actions[i].y - 5);
   }
-  checkAction(game.mousePosition)
+  checkAction(game.mousePosition);
+  animateText();
 }
 
 function checkAction(mouse) {
-  for (let i = 0; i < actions.length ; i++ ) {
+  for (let i = 0; i < actions.length; i++) {
     if (mouse.x > actions[i].x && mouse.x < actions[i].x + 145 && mouse.y < actions[i].y && mouse.y > actions[i].y - 45) {
-      console.log(actions[i].action)
+      if (oldSelection != undefined) actions[oldSelection].filter = "none";
+      actions[i].filter = filter;
+      oldSelection = i;
     }
   }
 }
+
+function animateText() {
+  tick += 1;
+  if (tick >= maxTick) {
+    tick = 0;
+    filter < 4 ? filter += 1 : filter = 0;
+  }
+}
+
