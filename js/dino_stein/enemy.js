fcef2a312ca1546f2a5fb8f00fefc7f60a32751d
Toto Briac <totobriac@gmail.com>
2022-01-03 02:45:58 +0100

    tricky DRY

--- js/dino_stein/enemy.js
+++ js/dino_stein/enemy.js
@@ -2,35 +2,37 @@ import {
   Sprite
 } from "./sprite.js";
 
+var type = "soldier_1";
+
 var soldier = new Image();
-soldier.src = "../assets/sewer_level/boss/still.png";
+soldier.src = "../assets/sewer_level/" + type + "/still.png";
 
 var walk_0 = new Image();
-walk_0.src = "../assets/sewer_level/boss/0.png";
+walk_0.src = "../assets/sewer_level/" + type + "/0.png";
 var walk_1 = new Image();
-walk_1.src = "../assets/sewer_level/boss/1.png";
+walk_1.src = "../assets/sewer_level/" + type + "/1.png";
 var walk_2 = new Image();
-walk_2.src = "../assets/sewer_level/boss/2.png";
+walk_2.src = "../assets/sewer_level/" + type + "/2.png";
 var walk_3 = new Image();
-walk_3.src = "../assets/sewer_level/boss/3.png";
+walk_3.src = "../assets/sewer_level/" + type + "/3.png";
 
 var shoot_0 = new Image();
-shoot_0.src = "../assets/sewer_level/boss/shoot_0.png";
+shoot_0.src = "../assets/sewer_level/" + type + "/shoot_0.png";
 var shoot_1 = new Image();
-shoot_1.src = "../assets/sewer_level/boss/shoot_1.png";
+shoot_1.src = "../assets/sewer_level/" + type + "/shoot_1.png";
 var shoot_2 = new Image();
-shoot_2.src = "../assets/sewer_level/boss/shoot_2.png";
+shoot_2.src = "../assets/sewer_level/" + type + "/shoot_2.png";
 
 var die_0 = new Image();
-die_0.src = "../assets/sewer_level/boss/die_0.png";
+die_0.src = "../assets/sewer_level/" + type + "/die_0.png";
 var die_1 = new Image();
-die_1.src = "../assets/sewer_level/boss/die_1.png";
+die_1.src = "../assets/sewer_level/" + type + "/die_1.png";
 var die_2 = new Image();
-die_2.src = "../assets/sewer_level/boss/die_2.png";
+die_2.src = "../assets/sewer_level/" + type + "/die_2.png";
 var die_3 = new Image();
-die_3.src = "../assets/sewer_level/boss/die_3.png";
+die_3.src = "../assets/sewer_level/" + type + "/die_3.png";
 var die_4 = new Image();
-die_4.src = "../assets/sewer_level/boss/die_4.png";
+die_4.src = "../assets/sewer_level/" + type + "/die_4.png";
 
 var enemies = [];
 
@@ -46,6 +48,11 @@ class Enemy extends Sprite {
     this.isInRange = false;
     this.isShot = false;
     this.pistol = pistol;
+    //this.type = type;
+    this.init();
+  }
+  init() {
+    type = "boss";
   }
   alert() {
     if (this.distance < 80) {
@@ -119,11 +126,9 @@ class Enemy extends Sprite {
   }
 }
 
-
 function createEnemies(player, ctx, level, pistol) {
   enemies[0] = new Enemy(300, 120, soldier, player, ctx, level,pistol);
 }
-
 
 function drawEnemies() {
   enemies.sort(function(obj1, obj2) {
