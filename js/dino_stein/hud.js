3a5ce395ff86e6a4ff31bb6d92754e68791e925f
Toto Briac <totobriac@gmail.com>
2022-01-04 18:33:30 +0100

    animated face

--- js/dino_stein/hud.js
+++ js/dino_stein/hud.js
@@ -12,6 +12,8 @@ class Hud {
     this.ctx = ctx;
     this.player = player;
     this.pistol = pistol;
+    this.faceColumn = 0;
+    this.faceLine = 0;
   }
   draw() {
     this.ctx.drawImage(hudSprite, 300, 325);
@@ -19,8 +21,8 @@ class Hud {
     this.ctx.font = "40px Wolf";
     this.ctx.fillText("1", 345, 385);
     this.ctx.fillText("1", 515, 385);
-    this.ctx.drawImage(gunSprite, 780,335);
-    this.ctx.drawImage(faceSprite, 556,340, 48,52);
+    this.ctx.drawImage(gunSprite, 780, 335);
+    this.ctx.drawImage(faceSprite, this.faceColumn * 40, this.faceLine * 46, 40, 46, 556, 340, 48, 52);
     this.update();
   }
   update() {
@@ -29,7 +31,26 @@ class Hud {
     this.ctx.fillText(this.player.life, 625, 385);
     this.ctx.fillText(this.player.score, 445, 385);
     this.ctx.fillText(this.pistol.ammo, 695, 385);
+    this.animateFace();
+  }
+  animateFace() {
+    if (this.player.life > 80) {
+      this.faceColumn = 0;
+      this.faceLine = 0;
+    } else if (this.player.life <= 80 && this.player.life > 50) {
+      this.faceColumn = 1;
+      this.faceLine = 0;
+    } else if (this.player.life <= 50 && this.player.life > 25) {
+      this.faceColumn = 0;
+      this.faceLine = 1;
+    } else if (this.player.life <= 25) {
+      this.faceColumn = 1;
+      this.faceLine = 1;
+    }
   }
 }
 
-export {Hud}
+
+export {
+  Hud
+}
