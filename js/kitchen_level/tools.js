62ced074dfffe17d88cdea4ad14d1287487195a9
Your Name <you@example.com>
2022-01-19 17:57:34 +0100

    snaping

--- js/kitchen_level/tools.js
+++ js/kitchen_level/tools.js
@@ -8,7 +8,7 @@ var potSprite = new Image();
 potSprite.src = "../assets/kitchen_level/pot.png";
 
 class Tool {
-  constructor(name, sprite, x, y, width, height, ctx) {
+  constructor(name, sprite, x, y, width, height, ctx, perfX, perfY, shadow) {
     this.name = name;
     this.sprite = sprite;
     this.x = x;
@@ -20,24 +20,45 @@ class Tool {
       x: null,
       y: null
     };
+    this.perfX = perfX;
+    this.perfY = perfY;
+    this.shadow = shadow;
   }
   draw() {
     this.ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
+  }
+  isClose() {
+    if (this.distance({ x: this.x, y: this.y }, { x: this.perfX, y: this.perfY }) < this.width / 3) {
+      this.x = this.perfX;
+      this.y = this.perfY;
+    }
+  }
+  distance(obj1, obj2) {
+    return Math.sqrt((obj1.x - obj2.x) * (obj1.x - obj2.x) +
+      (obj1.y - obj2.y) * (obj1.y - obj2.y))
+  }
+  drawShadow() {
+    this.ctx.beginPath();
+    this.ctx.fillStyle = "rgba(0, 0, 0, 0.17)";
+    this.ctx.arc(this.shadow.x, this.shadow.y, this.shadow.r, 0, 2 * Math.PI, false);
+    this.ctx.fill();
   }
 }
 
 function drawTools(ctx, game) {
   if (game.kitchenLevelStarted === false) {
-    pot = new Tool("pot", potSprite, 500, 20, 210, 161, ctx);
+    pot = new Tool("pot", potSprite, 500, 20, 210, 161, ctx, 24, 47, { x: 125, y: 130, r: 60 });
     tools.push(pot);
     game.kitchenLevelStarted = true;
   }
-  pot.draw();
-  if (selectedTool === pot) {
-    ctx.beginPath();
-    ctx.fillStyle = "rgba(0, 0, 0, 0.17)";
-    ctx.arc(125, 130, 60, 0, 2 * Math.PI, false);
-    ctx.fill();
+
+  if (selectedTool) {
+    selectedTool.drawShadow();
+  }
+
+  for (let i = 0; i < tools.length; i++) {
+    tools[i].draw();
+    tools[i].isClose();
   }
 }
 
