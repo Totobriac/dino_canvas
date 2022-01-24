5d146c79a1f18c11a52e67235f51982dab739e01
Your Name <you@example.com>
2022-01-24 17:41:02 +0100

    chopping board

--- js/kitchen_level/tools/table.js
+++ js/kitchen_level/tools/table.js
@@ -1,6 +1,10 @@
 var tableSprite = new Image();
-tableSprite.src = "../assets/kitchen_level/cutting_board_black.png"
+tableSprite.src = "../assets/kitchen_level/work_top.png";
+
+var choppingBoardSprite = new Image();
+choppingBoardSprite.src = "../assets/kitchen_level/chopping_board.png";
 
 export function generateTable(ctx) {
-  ctx.drawImage(tableSprite, 0, 0)
+  ctx.drawImage(tableSprite, 0, 0);
+  ctx.drawImage(choppingBoardSprite, 380, 230, 245, 161);
 }
