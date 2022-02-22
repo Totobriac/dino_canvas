633b4c9d4855b6a2df504f43086ce986d48b1df5
Toto Briac <totobriac@gmail.com>
2022-02-22 01:17:44 +0100

    good to go

--- js/kitchen_level/tools/tinOpener.js
+++ js/kitchen_level/tools/tinOpener.js
@@ -5,14 +5,28 @@ import {
 var handleSprite = new Image();
 handleSprite.src = "../assets/kitchen_level/handle_tin_opener.png";
 
+var crankSprite = new Image();
+crankSprite.src = "../assets/kitchen_level/crank_tin_opener.png";
+
 class TinOpener extends Tool {
   constructor(name, sprite, x, y, width, height, ctx, perfX, perfY, shadow) {
     super(name, sprite, x, y, width, height, ctx, perfX, perfY, shadow);
     this.isOpening = false;
+    this.angle = 0;
   }
   draw() {
     if (this.isOpening) {
+      this.angle ++;
       this.ctx.drawImage(handleSprite,530,100,396,105)
+
+      this.ctx.save();
+      this.ctx.translate(600,150);
+      this.ctx.rotate(-this.angle * Math.PI / 180);
+
+      this.ctx.drawImage(crankSprite,-80,-22,160,44)
+
+      this.ctx.restore();
+
     } else {
       super.draw();
     }
