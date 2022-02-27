687013f5c02062dfe50bc19fb531c9c041a869d7
Your Name <you@example.com>
2022-02-27 18:46:06 +0100

    cooking pastas

--- js/kitchen_level/tools/pasta.js
+++ js/kitchen_level/tools/pasta.js
@@ -2,17 +2,31 @@ import {
   Tool
 } from "./tool.js";
 
-import {
-  deleteTool,
-} from "../tools.js";
 
 var pastaSprite = new Image();
 pastaSprite.src = "../assets/kitchen_level/pasta.png";
+
+class singlePasta {
+  constructor() {
+    this.length = 85 + Math.floor(Math.random() * 10);
+    this.color = 60 +  Math.floor(Math.random() * 14); 
+  }
+}
 
 class Pasta extends Tool {
   constructor(name, sprite, x, y, width, height, ctx, perfX, perfY, shadow) {
     super(name, sprite, x, y, width, height, ctx, perfX, perfY, shadow);
     this.isUp = true;
+    this.pastas = [];
+    this.populatePastas();
+    this.count = 0;
+    this.maxCount = 12;
+    this.size = 0;
+  }
+  populatePastas() {
+    for (let i = 0; i < 76; i++) {
+      this.pastas.push(new singlePasta);
+    }
   }
   draw() {
     if (this.isSelected) this.isUp = false;
@@ -25,30 +39,40 @@ class Pasta extends Tool {
     }    
   }
   inPot() {
+    if( this.count > this.maxCount ) {
+      this.size ++;
+      this.count = 0;
+    }
+    else{
+      this.count ++;
+    };
+
     var start = 0;
     var end = 2 * Math.PI;
 
     let step = 0;
-    var totalSteps = 76;
-    var stepSize = (end - start) / totalSteps;    
-
-    for (let i = 0; i < 76; i++) {
-
+    var totalSteps = this.pastas.length;
+    var stepSize = (end - start) / totalSteps;   
+    
+    this.pastas.forEach(pasta => {
       var angle = start + step++ * stepSize;
+      var pastaLength = -pasta.length + this.size;
+      console.log(-pasta.length, this.size);
 
       this.ctx.save();
-      this.ctx.strokeStyle = "hsla(57, 100%, 67%, 1)";
+      this.ctx.strokeStyle = "hsla(57, 100%," + pasta.color +"%, 1)";
       this.ctx.lineWidth = 2;
       this.ctx.translate(1106, 173);
       this.ctx.rotate(+angle);
 
       this.ctx.beginPath();
       this.ctx.moveTo(10, -25);
-      this.ctx.lineTo(-90, -5);
+      this.ctx.lineTo(pastaLength, -5);
       this.ctx.stroke();
 
       this.ctx.restore();
-    }
+    });
+
   }
 }
 
