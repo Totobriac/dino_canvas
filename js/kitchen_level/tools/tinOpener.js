4554c4b5c0b9a476f0bfa1187801120331d8188d
Toto Briac <totobriac@gmail.com>
2022-02-28 03:58:35 +0100

    open tin

--- js/kitchen_level/tools/tinOpener.js
+++ js/kitchen_level/tools/tinOpener.js
@@ -28,13 +28,13 @@ class TinOpener extends Tool {
 
       this.isSelected = true;
 
-      this.ctx.drawImage(handleSprite, 530, 100, 396, 105);
-
       this.ctx.beginPath();
       this.ctx.strokeStyle = "red";
       this.ctx.lineWidth = 3;
-      this.ctx.ellipse(600, 125, 25, 106, 90 * Math.PI / 180,  2 * Math.PI ,-10 * Math.PI / 180);
+      this.ctx.ellipse(600, 125, 25, 106, 90 * Math.PI / 180,  0 ,-this.angle /2 * Math.PI / 180,true);
       this.ctx.stroke();
+
+      this.ctx.drawImage(handleSprite, 530, 100, 396, 105);
 
       this.ctx.save();
       this.ctx.translate(600, 150);
@@ -70,6 +70,8 @@ class TinOpener extends Tool {
           }
         }
       }
+
+      if (this.angle > 740) this.isOpening = false
     } else {
       super.draw();
     }
