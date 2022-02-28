b16e25f1ea435547e94a0456cfd495f3778e0aa8
Toto Briac <totobriac@gmail.com>
2022-02-28 03:16:33 +0100

    weird with butter ??

--- js/kitchen_level/tools/pasta.js
+++ js/kitchen_level/tools/pasta.js
@@ -9,7 +9,11 @@ pastaSprite.src = "../assets/kitchen_level/pasta.png";
 class singlePasta {
   constructor() {
     this.length = 85 + Math.floor(Math.random() * 10);
-    this.color = 60 +  Math.floor(Math.random() * 14); 
+    this.color = 60 + Math.floor(Math.random() * 14);
+    this.start = Math.floor(Math.random() * 10);
+    this.five = Math.floor(Math.random() * 5);
+    // this.bezier = Math.random() < 0.5 ? -1 : 1;
+    this.bezier = -2 + Math.floor(Math.random() * 4);
   }
 }
 
@@ -22,9 +26,10 @@ class Pasta extends Tool {
     this.count = 0;
     this.maxCount = 12;
     this.size = 0;
+    this.shriking = true;
   }
   populatePastas() {
-    for (let i = 0; i < 76; i++) {
+    for (let i = 0; i < 150; i++) {
       this.pastas.push(new singlePasta);
     }
   }
@@ -33,47 +38,52 @@ class Pasta extends Tool {
     if (!this.isUp) {
       this.sprite = pastaSprite;
       super.draw();
-    }
-    else {
+    } else {
       super.draw();
-    }    
+    }
   }
   inPot() {
-    if( this.count > this.maxCount ) {
-      this.size ++;
+    if (this.count > this.maxCount && this.size < 35) {
+      this.size++;
       this.count = 0;
-    }
-    else{
-      this.count ++;
+    } else {
+      this.count++;
     };
-
+    console.log(this.size);
     var start = 0;
     var end = 2 * Math.PI;
 
     let step = 0;
     var totalSteps = this.pastas.length;
-    var stepSize = (end - start) / totalSteps;   
-    
+    var stepSize = (end - start) / totalSteps;
+
     this.pastas.forEach(pasta => {
       var angle = start + step++ * stepSize;
       var pastaLength = -pasta.length + this.size;
-      console.log(-pasta.length, this.size);
+
 
       this.ctx.save();
-      this.ctx.strokeStyle = "hsla(57, 100%," + pasta.color +"%, 1)";
+      this.ctx.strokeStyle = "hsla(57, 100%," + pasta.color + "%, 1)";
       this.ctx.lineWidth = 2;
       this.ctx.translate(1106, 173);
       this.ctx.rotate(+angle);
-
-      this.ctx.beginPath();
-      this.ctx.moveTo(10, -25);
-      this.ctx.lineTo(pastaLength, -5);
-      this.ctx.stroke();
-
+      if (this.size < 31) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(pasta.start, -pasta.five * 5);
+        this.ctx.lineTo(pastaLength, -pasta.five);
+        this.ctx.stroke();
+      } else {
+        this.ctx.beginPath();
+        this.ctx.moveTo(pasta.start, -pasta.five * 5);
+        this.ctx.quadraticCurveTo(pasta.bezier * this.size ,pasta.bezier * this.size * -1 , pastaLength, -pasta.five);
+        this.ctx.stroke();
+      }
       this.ctx.restore();
     });
 
   }
 }
 
-export { Pasta };
+export {
+  Pasta
+};
