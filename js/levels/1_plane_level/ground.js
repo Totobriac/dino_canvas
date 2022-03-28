60c9b624989f9b2a836740d12642ae9d461e5269
Toto Briac <totobriac@gmail.com>
2022-03-28 02:44:43 +0200

    monu change ok

--- js/levels/1_plane_level/ground.js
+++ js/levels/1_plane_level/ground.js
@@ -1,5 +1,10 @@
-const statue = new Image();
-statue.src = "./assets/1_plane/russian.png";
+var monuIndex = 0;
+
+const monuSprites = ["./assets/1_plane/liberty.png","./assets/1_plane/pagoda.png","./assets/1_plane/cn_tower.png",
+                    "./assets/1_plane/scottish.png","./assets/1_plane/thai.png","./assets/1_plane/russian.png"];
+
+var statue = new Image();
+
 
 const sWidth = 200;
 const sHeight = 200;
@@ -10,23 +15,16 @@ const start = 0.9272951769;
 const end = 2.21429922;
 
 let step = 0;
-const totalSteps = 800;
+const totalSteps = 700;
 const stepSize = (end - start)/totalSteps;
 
 export function animateMonument(ctx) {
 
-  if (step === totalSteps) step = 0;
+  if (step === totalSteps) {
+    step = 0;
+    monuIndex === monuSprites.length - 1 ? monuIndex = 0 : monuIndex  ++;
+  }
   const angle = start + step++ * stepSize;
-
-  // ctx.translate(origin.x, origin.y);
-  // ctx.rotate(-angle);
-
-  // ctx.translate(radius, 0);
-  // ctx.rotate(Math.PI/2);
-
-  // ctx.translate(-sWidth/2, -sHeight/2);
-  // ctx.drawImage(statue, 0, 0, sWidth, sHeight);
-  // ctx.resetTransform();
 
   ctx.translate(origin.x, origin.y);
   drawCircle(0, 50, 900, ctx);
@@ -41,6 +39,9 @@ export function animateMonument(ctx) {
   ctx.rotate(Math.PI/2);
 
   ctx.translate(-sWidth/2, -sHeight/1.4);
+
+  statue.src = monuSprites[monuIndex];
+
   ctx.drawImage(statue, 0, 0, sWidth, sHeight);
   ctx.resetTransform();
 }
