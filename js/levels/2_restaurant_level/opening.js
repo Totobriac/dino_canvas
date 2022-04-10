452d2f38629ebd792edc2e4ebe9c57c4152ad016
Your Name <you@example.com>
2022-04-10 21:07:59 +0200

    cook

--- js/levels/2_restaurant_level/opening.js
+++ js/levels/2_restaurant_level/opening.js
@@ -16,6 +16,13 @@ rightDoorSprite.src = "./assets/2_restaurant/right_door.png";
 var rampSprite = new Image();
 rampSprite.src = "./assets/2_restaurant/rampe_original.png";
 
+var cookSprite = new Image();
+cookSprite.src = "./assets/2_restaurant/inside_no_door_big.png";
+
+
+const cookySprite = new Image();
+cookySprite.src = "./assets/2_restaurant/cook.png";
+
 var winHeight = window.innerHeight;
 var winWidth = window.innerWidth;
 
@@ -32,6 +39,14 @@ const seaAnim = {
   ticksPerFrame: 12,
   tickCount: 0,
 }
+
+const cook = {
+  frames: 8,
+  frameIndex: 0,
+  ticksPerFrame: 12,
+  tickCount: 0,
+}
+
 
 function drawOpening(ctx, left, top) {
 
@@ -52,17 +67,20 @@ function drawOpening(ctx, left, top) {
       xOffset++;
     } else if (xOffset === 200 && doorOffset > 0) {
       closeDoor = true;
-      doorOffset-= 0.5;
+      doorOffset -= 0.5;
     }
 
-    ctx.drawImage(skySprite, 0, 0, 1200, 820, left - 600 + (xOffset * 3), top - 1500 + (xOffset * 4.1), 2400 - (xOffset * 6), 1640 - (xOffset * 4.1), );
+    ctx.drawImage(skySprite, 0, 0, 1200, 820, left - 600 + (xOffset * 3), top - 1500 + (xOffset * 4.1), 2400 - (xOffset * 6), 1640 - (xOffset * 4.1),);
     animSea();
     ctx.drawImage(seaSprite, 300 - (xOffset * 1.5), 20 + (241 * seaAnim.frameIndex), 600 + (xOffset * 3), 110 + (xOffset * 0.55), left, top + 140, 1200, 260);
     ctx.drawImage(rampSprite, 592 - (xOffset * 1.36), 0, 77 + (xOffset * 2.615), 26 + (xOffset * 0.87), left, top + 199 - (xOffset * 0.2), 1200, 400);
 
-    if( closeDoor) drawDoor(ctx, left, top);
+    if (closeDoor) drawDoor(ctx, left, top);
     ctx.drawImage(restBackSprite, 592 - (xOffset * 1.36), 54 - (xOffset * 0.27), 77 + (xOffset * 2.615), 26 + (xOffset * 0.87), left, top, 1200, 400);
 
+    ctx.drawImage(cookSprite, 1184 - (xOffset * 2.72), 108 - (xOffset * 0.54), 154 + (xOffset * 5.23), 52 + (xOffset * 1.74), left, top, 1200, 400);
+
+    drawChar(ctx, left, top);
   }
   drawBottMask(ctx, top);
 }
@@ -92,6 +110,14 @@ function checkFrame(sprite) {
       sprite.frameIndex = 0;
     }
   }
+}
+
+function drawChar(ctx, left, top) {
+  // cook.tickCount += 1;
+  // checkFrame(cook);
+  ctx.drawImage(cookySprite, (50 * cook.frameIndex), 0, 50, 100, 1000 + left + 60, 155 + top, 60, 120);
+  //ctx.drawImage(cookSprite, 1184 - (xOffset * 2.72), 108 - (xOffset * 0.54), 154 + (xOffset * 2.615), 26 + (xOffset * 0.87), left, top, 1200, 400);
+
 }
 
 export {
