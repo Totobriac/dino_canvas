9cfdb6c00832fdce2ebb9c2e3fb665c4320a0b3b
Toto Briac <totobriac@gmail.com>
2022-04-27 01:35:04 +0200

    trying to melt

--- js/levels/5_bridge_level/startLevel5.js
+++ js/levels/5_bridge_level/startLevel5.js
@@ -9,6 +9,20 @@ arrowsKeys.src = "./assets/5_bridge/arrows.png";
 
 var circleD = 0;
 var start = false;
+var getData = false;
+var imgData;
+var imageData;
+
+var tempCanvas = document.createElement('canvas');
+tempCanvas.width = 1200;
+tempCanvas.height = 400;
+var tempContext = tempCanvas.getContext('2d');
+
+tempContext.fillStyle = "white";
+tempContext.fillRect(0, 0, 1200, 400);
+
+imageData = tempContext.getImageData(0, 0, canvas.width, canvas.height);
+
 
 window.addEventListener('mousedown', function () {
   startGame();
@@ -35,6 +49,20 @@ export function startLevel(ctx, game, dino) {
     generateBridge(ctx);
     generatePiano(ctx, game.frame);
     generateRain(ctx, game);
+    console.log(imageData.data.length);
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      imageData.data[i] = 255;
+      imageData.data[i + 1] = 255;
+      imageData.data[i + 2] = 255;
+      imageData.data[i + 3] = 255;
+    }
+
+    tempContext.putImageData(imageData, 0, 0);
+
+    var img = new Image();
+    img.src = tempCanvas.toDataURL();
+    ctx.drawImage(tempCanvas, 0, 0);
 
   }
 }
