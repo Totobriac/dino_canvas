f8699550ffc442b92e11f4dd3a31b60705520464
your_name <your_name@hotmail.com>
2022-08-28 08:50:13 +0200

    need circle on top of scen 2

--- js/levels/4_submarine_level/finalScene.js
+++ js/levels/4_submarine_level/finalScene.js
@@ -16,6 +16,9 @@ exit.src = "./assets/4_submarine/indy_exit.png";
 var maze = new Image();
 maze.src = "./assets/4_submarine/maze.png";
 
+var topMaze = new Image();
+topMaze.src = "./assets/4_submarine/topMaze.png";
+
 var subRight = new Image();
 subRight.src = "./assets/4_submarine/empty_submarine.png";
 
@@ -25,8 +28,13 @@ subJet.src = "./assets/4_submarine/bubble_jet.png";
 var dino = new Image();
 dino.src = "./assets/dino/dino_walk_left.png";
 
+var dino2 = new Image();
+dino2.src = "./assets/dino/dino_walk.png";
+
 var exitingSub;
 var exitingDino;
+
+var scene = 2;
 
 class ExitingDino {
   constructor(sprite, ctx, exitingSub) {
@@ -36,26 +44,54 @@ class ExitingDino {
     this.tickount = 0;
     this.dinoFrame = 1;
     this.sub = exitingSub;
+    this.x = 580;
+    this.y = 350;
+    this.path = 0;
   }
   draw() {
     this.update();
-    this.ctx.drawImage(dino, 90 * this.dinoFrame, 0, 90, 99, this.x, this.y, 27, 30);
+    this.ctx.drawImage(this.sprite, 90 * this.dinoFrame, 0, 90, 99, this.x, this.y, 27, 30);
   }
   update() {
     if (this.tickount > this.maxTicount) {
       this.dinoFrame === 1 && !this.engine ? this.dinoFrame = 0 : this.dinoFrame = 1;
       this.tickount = 0;
     }
-    if (!this.sub.engine) this.tickount++;
+    if (scene === 1) {
+      if (!this.sub.engine) this.tickount++;
 
-    if (this.sub.engine) {
-      this.x = this.sub.x + 12;
-      this.y = this.sub.y + 35;
-    } else if (this.y > 132 || this.x > 196) {
-      if (this.y > 132) this.y -= 0.5;
-      if (this.x > 196) this.x -= 0.5;
-    } else {
-      closeCurtain();
+      if (this.sub.engine) {
+        this.x = this.sub.x + 12;
+        this.y = this.sub.y + 35;
+      } else if (this.y > 132 || this.x > 196) {
+        if (this.y > 132) this.y -= 5;
+        if (this.x > 196) this.x -= 5;
+      } else {
+        if (curtain1.isOpen) {
+          closeCurtain();
+        } else {
+          this.x = 585;
+          this.y = 390;
+          switchScene(2);
+        }
+      }
+    }
+    else if (scene === 2) {
+      this.tickount++;
+
+      if (this.path === 0 && this.x > 516 || this.y > 277) {
+        if (this.x > 516) this.x -= 0.22;
+        if (this.y > 277) this.y -= 0.2;
+      } else {
+        this.path = 1
+      }
+
+      if (this.path === 1) {
+        this.sprite = dino2;
+        console.log(this.x);
+        if (this.x < 715) this.x += 0.17;
+        if (this.y > 52) this.y -= 0.2;
+      }
     }
   }
 }
@@ -100,6 +136,7 @@ var curtain1 = {
   y: 0,
   height: 400,
   width: 600,
+  isOpen: true,
 }
 
 var curtain2 = {
@@ -107,11 +144,21 @@ var curtain2 = {
   y: 0,
   height: 400,
   width: 600,
+  isOpen: false,
 }
 
 function closeCurtain() {
-  if (curtain1.x < 0) curtain1.x += 1.6;
-  if (curtain2.x > 600) curtain2.x -= 1.6;
+  curtain1.x < 0 ? curtain1.x += 1 : curtain1.isOpen = false;
+  curtain2.x > 600 ? curtain2.x -= 1 : curtain2.isOpen = false;
+}
+
+function openCurtain() {
+  curtain1.x > -198 ? curtain1.x -= 1 : curtain1.isOpen = true;
+  curtain2.x < 1002 ? curtain2.x += 1 : curtain2.isOpen = true;
+}
+
+function switchScene(n) {
+  scene = n;
 }
 
 function drawFinalScene(ctx) {
@@ -119,14 +166,27 @@ function drawFinalScene(ctx) {
   if (!exitingSub) exitingSub = new ExitingSub(920, 260, subRight, ctx);
   if (!exitingDino) exitingDino = new ExitingDino(dino, ctx, exitingSub);
 
-  ctx.drawImage(entrance, 198, 0);
 
-  exitingDino.draw();
-  exitingSub.draw();
+  if (scene === 1) {
 
-  ctx.drawImage(rock, 198, 0);
-  ctx.drawImage(waterBottom, 198, 0);
-  ctx.drawImage(waterTop, 198, 0);
+    ctx.drawImage(entrance, 198, 0);
+
+    exitingDino.draw();
+    exitingSub.draw();
+
+    ctx.drawImage(rock, 198, 0);
+    ctx.drawImage(waterBottom, 198, 0);
+    ctx.drawImage(waterTop, 198, 0);
+
+  } else if (scene === 2) {
+
+    if (!curtain1.isOpen) openCurtain();
+    ctx.drawImage(maze, 0, 0);
+    exitingDino.draw();
+    ctx.drawImage(topMaze, 0, 0);
+
+
+  }
 
   ctx.fillRect(curtain1.x, curtain1.y, curtain1.width, curtain1.height);
   ctx.fillRect(curtain2.x, curtain2.y, curtain2.width, curtain2.height);
