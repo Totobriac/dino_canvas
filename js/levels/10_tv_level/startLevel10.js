2084d0e6626a3fef591d3070b6b47b32c15ca466
Toto Briac <totobriac@gmail.com>
2022-09-05 04:54:19 +0200

    look who is here

--- js/levels/10_tv_level/startLevel10.js
+++ js/levels/10_tv_level/startLevel10.js
@@ -11,15 +11,20 @@ mouseKeys.src = "./assets/3_kitchen/left_mouse.png";
 var overLay = new Image();
 overLay.src = "./assets/10_tv/tvOverlay.png";
 
+var mire = new Image();
+mire.src = "./assets/10_tv/mire.png";
+
 var circleD = 0;
 var start = false;
+
+var channel1 = false;
 
 window.addEventListener('mousedown', function () {
   startGame();
 })
 
 export function startLevel(game, ctx) {
- 
+
   if (circleD < 60 && !start) circleD += 0.5;
   if (circleD >= 0.5 && start) circleD -= 0.5;
   if (circleD === 0 && start) game.start = true;
@@ -32,21 +37,32 @@ export function startLevel(game, ctx) {
   ctx.fill();
   ctx.restore();
 
+
+
   if (game.start) {
-    
+
     var channel = buttonsChoice(game.mousePosition);
+    console.log(channel1);
     switch (channel) {
       case 0:
         generateNoise(ctx);
         break;
       case 1:
-        video.play();
-        playVideoPauleta(ctx);
+        if (channel1) {
+          video.pause();
+          video.currentTime = 0;
+          playWheelGame(ctx);
+        } else {
+          ctx.drawImage(mire, 240,0);
+        }
         break;
       case 2:
-        video.pause();
-        video.currentTime = 0;        
-        playWheelGame(ctx);
+        ctx.drawImage(mire, 240,0);
+        break;
+      case 3:
+        channel1 = true;
+        video.play();
+        playVideoPauleta(ctx);
         break;
     }
     ctx.drawImage(overLay, 0, 0);
